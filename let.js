61e137c5394ec3815ae0368509ef624c91cfa949
Jotappe23 <joaopdrprof23@gmail.com>
2026-01-19 22:04:13 -0300

    aula-js

--- let.js
+++ let.js
@@ -10,3 +10,4 @@ console.log(idade);
 idade = 15.5
 
 console.log(idade);
+
---
+++ login/script.js
@@ -0,0 +1,11 @@
+console.log('login');
+
+const emailInput = document.getElemnetById('email').value;
+const senhaInput = document.getElemnetById('senha').value;
+
+console.log(emailInput);
+console.log(senhaInput);
+
+function login() {
+    console.log('clicou no login');
+}
