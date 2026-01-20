9d6792a7172c1b3766eb7545f81a1800e0f0b573
Jotappe23 <joaopdrprof23@gmail.com>
2026-01-19 21:07:33 -0300

    aula-19/01/2026

---
+++ tipos_dados.js
@@ -0,0 +1,26 @@
+// declarando a vaiável
+let cor;
+
+// atribuindo um valor inicial
+cor = 'azul';
+
+// imprimindo um console
+console.log(cor);
+
+let cidade = 'Fortaleza';
+
+// tipo de dado booleano (true ou false)
+let ehEstrangeiro = true;
+
+let listaTamanho = ['P','M','G', 42, 50];
+
+ehEstrangeiro = 'Inglaterra';
+
+console.log(ehEstrangeiro);
+
+listaTamanho.push(false);
+
+console.log(listaTamanho);
+
+// atributos e funções tipos de dados
+console.log(cor.length);
---
+++ variaveis.js
@@ -0,0 +1,11 @@
+//criamos uma constante com um valor inicial
+const nome = 'João Pedro';
+
+//Imprimir o valor da variável
+console.log(nome);
+
+//Atualizar o valor
+nome = 'João';
+
+//Imprimir o valor atualizado
+console.log(nome);
